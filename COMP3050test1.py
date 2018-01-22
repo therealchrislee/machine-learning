2f451b968ed2780d62092643996da3b5e59aca7a
Chris Lee <therealchrislee@me.com>
2018-01-21 23:20:10 -0600

    Cleanup

---
+++ COMP3050test1.py
@@ -0,0 +1,13 @@
+country_counter = {}
+
+def addone(country):
+    if country in country_counter:
+        country_counter[country] += 1
+    else:
+        country_counter[country] = 1
+
+addone('China')
+addone('Japan')
+addone('china')
+
+print len(country_counter)
